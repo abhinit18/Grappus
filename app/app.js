002266421f4f98e1bf138b8f628cf8de22ee1283
unknown <abhinit.roy@clicklabs.co>
2016-09-20 15:29:56 +0530

    changes

--- app/app.js
+++ app/app.js
@@ -42,29 +42,6 @@ App.controller('UserController', function ($scope, $http, $cookies, $cookieStore
 });
 
 
-//service for scored data//
-
-//
-//App.service('ScoreData', function($http){
-//    var promise;
-//    var myService = {
-//        async: function() {
-//            if ( !promise ) {
-//                // $http returns a promise, which has a then function, which also returns a promise
-//                promise = $http.get('vendor/sachin').then(function (response) {
-//                    // The then function here is an opportunity to modify the response
-//                    console.log(response);
-//                    // The return value gets picked up by the then in the controller.
-//                    return response.data;
-//                });
-//            }
-//            // Return the promise to the controller
-//            return promise;
-//        }
-//    };
-//    return myService;
-//
-//});
 
 
 
